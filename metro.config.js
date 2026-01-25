3a11a28e31c079f83af38d6c1e623965ef802daf
p18khr <prakharpps.18@gmail.com>
2026-01-25 22:10:18 +0530

    Final prep for EAS: Tracked native android folder and applied fixes

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,9 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
 
-/** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
+
+// Fix for Firebase Auth in Expo 53 / SDK 53
+config.resolver.sourceExts.push('cjs');
+config.resolver.unstable_enablePackageExports = false;
 
 module.exports = config;
