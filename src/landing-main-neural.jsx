11f99dbdada4c47411bfe05131b49404021721ba
p18khr <prakharpps.18@gmail.com>
2026-04-02 21:31:17 +0530

    Revert "new landing page"
    This reverts commit 97d06f7f6485bde73da633c79b5c68ddd6e62801.

--- src/landing-main-neural.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import LandingPageNeural from './components/LandingPageNeural';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <LandingPageNeural />
-  </React.StrictMode>
-);
